2f559a3221d2b5594ed4ff7e170fa5dbd223ddec
evxeen <evxeen@gmail.com>
2021-08-21 18:58:30 +0300

    add: Map

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-console.log('hi');
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        console.log(latitude);
+        console.log(longitude);
+
+        const coords = [latitude, longitude]
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker( coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+    }, function () {
+        alert('Не удается получить ваше местоположение 😢');
+    });
+}
