b32fa45dbfcf96eaacfd8e976a51df0b392e3848
evxeen <evxeen@gmail.com>
2021-08-24 12:20:41 +0300

    add: rend input form

--- script.js
+++ script.js
@@ -11,37 +11,54 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
 
-        const coords = [latitude, longitude]
+        const coords = [latitude, longitude];
 
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        map.on('click', function (mapEvent) {
-            console.log(mapEvent);
-            const {lat, lng} = mapEvent.latlng;
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
 
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                }))
-                .setPopupContent('Тренировка')
-                .openPopup();
+
         }, function () {
             alert('Не удается получить ваше местоположение 😢');
         });
-        })
-
-
+    });
 }
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    console.log(mapEvent);
+    const {lat, lng} = mapEvent.latlng;
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+        .setPopupContent('Тренировка')
+        .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
